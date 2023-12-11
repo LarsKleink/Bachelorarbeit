a6a6a28878b90aa48a3c83e1ccbbf156091719d2
Lars <l.kleinkop@posteo.de>
2023-12-11 18:37:10 +0100

    Added directory Datasets, renamed directory Algorithms

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # main file
+import toml
+
+with open('config.toml', 'r') as f:
+    config = toml.load(f)
+
+for x in range(config["algorithms"]["count_a"]):
+    print(config["algorithms"][str(x)])
