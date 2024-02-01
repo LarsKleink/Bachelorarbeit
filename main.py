e99157e5a4c5e61f52ba816dd2fe0316cb1c42fb
Lars <l.kleinkop@posteo.de>
2024-02-01 20:00:58 +0100

    added BUFF execution

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import os, os.path
 with open('config.toml', 'r') as f:
     config = toml.load(f)
 
-btrblocks_location = "~/prj/BtrBlocks/btrblocks/build/my_compression"
-elf_location = chimp_location = gorilla_location = "./Algorithms/ELF/out/artifacts/start_compress_jar/start-compress.jar"
-buff_location = "./Algorithms/BUFF"
+btrblocks_location = "~/prj/BtrBlocks/build/my_compression"
+elf_location = chimp_location = gorilla_location = "~/prj/ELF/out/artifacts/start_compress_jar/start-compress.jar"
+buff_location = "~/prj/BUFF/database"
 
 for x in range(config["algorithms"]["count_a"]):
     current_algorithm = config["algorithms"][str(x)].lower()
@@ -34,7 +34,7 @@ for x in range(config["algorithms"]["count_a"]):
             case "gorilla":
                 execute_string += "java -jar " + elf_location + " gorilla " + fl + ".csv"
             case "buff":
-                execute_string += buff_location + " " + fl
+                execute_string += "cargo +nightly run --manifest-path " + buff_location + "/Cargo.toml --release  --package buff --bin comp_profiler " + fl + " buff 10000 1.1509"
         print(execute_string)
         subprocess.run(execute_string, shell=True)
         print("successfully executed " + current_algorithm + " with dataset " + current_dataset + "\n")
@@ -42,3 +42,6 @@ for x in range(config["algorithms"]["count_a"]):
 
 
 # subprocess.run("~/prj/BtrBlocks/btrblocks/build/my_compression ~/prj/BtrBlocks/btrblocks/datasets/BUFF-datasetbin", shell=True)
+# cargo run --manifest-path /home/lars/prj/BUFF/database/Cargo.toml --bin comp_profiler
+# cargo +nightly run --manifest-path /home/lars/prj/BUFF/database/Cargo.toml --release  --package buff --bin comp_profiler ./data/randomwalkdatasample1k-1k buff-simd 10000 1.1509
+
